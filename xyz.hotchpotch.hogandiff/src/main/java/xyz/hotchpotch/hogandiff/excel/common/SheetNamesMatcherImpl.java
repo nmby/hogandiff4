3eb860eb90966fa7dc0224cc38b08137ca520dd3
nmby <nmby@hotchpotch.xyz>
2023-09-22 23:19:27 +0900

    SheetNamesMatcher, BookNamesMatcherのインタフェース化

---
+++ xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/excel/common/SheetNamesMatcherImpl.java
@@ -0,0 +1,64 @@
+package xyz.hotchpotch.hogandiff.excel.common;
+
+import java.util.List;
+import java.util.Objects;
+
+import xyz.hotchpotch.hogandiff.core.Matcher;
+import xyz.hotchpotch.hogandiff.core.StringDiffUtil;
+import xyz.hotchpotch.hogandiff.excel.BookInfo;
+import xyz.hotchpotch.hogandiff.excel.SheetNamesMatcher;
+import xyz.hotchpotch.hogandiff.util.IntPair;
+import xyz.hotchpotch.hogandiff.util.Pair;
+
+/**
+ * {@link SheetNamesMatcher} の標準的な実装です。<br>
+ * 
+ * @author nmby
+ */
+public class SheetNamesMatcherImpl implements SheetNamesMatcher {
+    
+    // [static members] ********************************************************
+    
+    /**
+     * 2つのExcelブックに含まれるシート名同士の対応関係を決めるマッチャーを返します。<br>
+     * 
+     * @param matchNamesStrictly シート名の揺らぎを許容しない場合は {@code true}
+     * @return シート名同士の対応関係を決めるマッチャー
+     */
+    public static SheetNamesMatcherImpl of(boolean matchNamesStrictly) {
+        return new SheetNamesMatcherImpl(matchNamesStrictly
+                ? Matcher.identityMatcher()
+                : Matcher.nerutonMatcherOf(
+                        String::length,
+                        StringDiffUtil::levenshteinDistance));
+    }
+    
+    // [instance members] ******************************************************
+    
+    private Matcher<String> coreMatcher;
+    
+    private SheetNamesMatcherImpl(Matcher<String> coreMatcher) {
+        assert coreMatcher != null;
+        
+        this.coreMatcher = coreMatcher;
+    }
+    
+    @Override
+    public List<Pair<String>> pairingSheetNames(
+            BookInfo bookInfo1,
+            BookInfo bookInfo2) {
+        
+        Objects.requireNonNull(bookInfo1, "bookInfo1");
+        Objects.requireNonNull(bookInfo2, "bookInfo2");
+        
+        List<IntPair> pairs = coreMatcher.makePairs(
+                bookInfo1.sheetNames(),
+                bookInfo2.sheetNames());
+        
+        return pairs.stream()
+                .map(p -> Pair.ofNullable(
+                        p.hasA() ? bookInfo1.sheetNames().get(p.a()) : null,
+                        p.hasB() ? bookInfo2.sheetNames().get(p.b()) : null))
+                .toList();
+    }
+}
