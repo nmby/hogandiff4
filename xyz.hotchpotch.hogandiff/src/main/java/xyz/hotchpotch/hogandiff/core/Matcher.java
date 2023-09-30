99aea1112f58c67f6ad9227ab9bfaa21bfb6c1a0
nmby <nmby@hotchpotch.xyz>
2023-09-30 12:46:25 +0900

    BookNamesMatcher実装の改善

--- xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/core/Matcher.java
+++ xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/core/Matcher.java
@@ -2,6 +2,7 @@ package xyz.hotchpotch.hogandiff.core;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.function.ToIntBiFunction;
 import java.util.function.ToIntFunction;
 
@@ -31,23 +32,28 @@ public interface Matcher<T> {
     }
     
     /**
-     * 2つのリスト間の編集距離が最小となるように要素同士を対応付けるマッチャーを返します。<br>
+     * リスト内における要素の順番に関わりなく
+     * 2つのリストの等しい要素同士を対応付けるマッチャーを返します。<br>
      * 
      * @param <T> リストの要素の型
-     * @param gapEvaluator 余剰コスト評価関数
-     * @param diffEvaluator 差分コスト評価関数
      * @return 新しいマッチャー
-     * @throws NullPointerException
-     *              {@code gapEvaluator}, {@code diffEvaluator} のいずれかが {@code null} の場合
      */
-    public static <T> Matcher<T> minimumEditDistanceMatcherOf(
-            ToIntFunction<? super T> gapEvaluator,
-            ToIntBiFunction<? super T, ? super T> diffEvaluator) {
-        
-        Objects.requireNonNull(gapEvaluator, "gapEvaluator");
-        Objects.requireNonNull(diffEvaluator, "diffEvaluator");
-        
-        return new MinimumEditDistanceMatcher2<>(gapEvaluator, diffEvaluator);
+    public static <T> Matcher<T> identityMatcher() {
+        return new IdentityMatcher<>();
+    }
+    
+    /**
+     * リスト内における要素の順番に関わりなく
+     * 2つのリストの等しい要素同士を対応付けるマッチャーを返します。<br>
+     * 
+     * @param <T> リストの要素の型
+     * @param idExtractor 要素の等価性を判断するためのid抽出器
+     * @return 新しいマッチャー
+     * @throws NullPointerException {@code idExtractor} が {@code null} の場合
+     */
+    public static <T> Matcher<T> identityMatcher(Function<? super T, ?> idExtractor) {
+        Objects.requireNonNull(idExtractor, "idExtractor");
+        return new IdentityMatcher<>(idExtractor);
     }
     
     /**
@@ -72,14 +78,38 @@ public interface Matcher<T> {
     }
     
     /**
-     * リスト内における要素の順番に関わりなく
-     * 2つのリストの等しい要素同士を対応付けるマッチャーを返します。<br>
+     * 2つのリスト間の編集距離が最小となるように要素同士を対応付けるマッチャーを返します。<br>
      * 
      * @param <T> リストの要素の型
+     * @param gapEvaluator 余剰コスト評価関数
+     * @param diffEvaluator 差分コスト評価関数
      * @return 新しいマッチャー
+     * @throws NullPointerException
+     *              {@code gapEvaluator}, {@code diffEvaluator} のいずれかが {@code null} の場合
      */
-    public static <T> Matcher<T> identityMatcher() {
-        return new IdentityMatcher<>();
+    public static <T> Matcher<T> minimumEditDistanceMatcherOf(
+            ToIntFunction<? super T> gapEvaluator,
+            ToIntBiFunction<? super T, ? super T> diffEvaluator) {
+        
+        Objects.requireNonNull(gapEvaluator, "gapEvaluator");
+        Objects.requireNonNull(diffEvaluator, "diffEvaluator");
+        
+        return new MinimumEditDistanceMatcher2<>(gapEvaluator, diffEvaluator);
+    }
+    
+    public static <T> Matcher<T> minimumCostFlowMatcherOf(
+            ToIntFunction<? super T> gapEvaluator,
+            ToIntBiFunction<? super T, ? super T> diffEvaluator) {
+        
+        Objects.requireNonNull(gapEvaluator, "gapEvaluator");
+        Objects.requireNonNull(diffEvaluator, "diffEvaluator");
+        
+        return new MinimumCostFlowMatcher<>(gapEvaluator, diffEvaluator);
+    }
+    
+    public static <T> Matcher<T> combinedMatcher(List<Matcher<? super T>> matchers) {
+        Objects.requireNonNull(matchers, "matchers");
+        return new CombinedMatcher<>(matchers);
     }
     
     // [instance members] ++++++++++++++++++++++++++++++++++++++++++++++++++++++
