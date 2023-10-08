ff312a1a3de9d917cae5cec9342c43318b1f6f8b
nmby <nmby@hotchpotch.xyz>
2023-10-08 12:44:58 +0900

    バージョン更新（v0.17.1 -> 0.17.2）

--- xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/AppMain.java
+++ xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/AppMain.java
@@ -21,7 +21,7 @@ public class AppMain extends Application {
     // [static members] ********************************************************
     
     /** このアプリケーションのバージョン */
-    private static final String VERSION = "v0.17.1";
+    private static final String VERSION = "v0.17.2";
     
     /** このアプリケーションのドメイン（xyz.hotchpotch.hogandiff） */
     public static final String APP_DOMAIN = AppMain.class.getPackageName();
