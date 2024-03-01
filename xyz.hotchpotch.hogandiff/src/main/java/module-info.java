68c75b95c3948f446786e65ca4d606463a9a8b03
nmby <nmby@hotchpotch.xyz>
2024-03-02 01:42:45 +0900

    add module-info.java

---
+++ xyz.hotchpotch.hogandiff/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module xyz.hotchpotch.hogandiff {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.desktop;
+    requires org.apache.poi.ooxml;
+}
