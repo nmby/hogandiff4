22d9c434df88dd605e5499452100973f5f341bf8
nmby <nmby@hotchpotch.xyz>
2024-03-02 01:51:40 +0900

    必要なはずのモジュールを追加

--- xyz.hotchpotch.hogandiff/src/main/java/module-info.java
+++ xyz.hotchpotch.hogandiff/src/main/java/module-info.java
@@ -1,7 +1,13 @@
 module xyz.hotchpotch.hogandiff {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires javafx.controls;
     requires java.desktop;
+    requires java.management;
+    requires java.naming;
+    requires java.xml;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires jdk.charsets;
+    requires jdk.zipfs;
     requires org.apache.poi.ooxml;
 }
