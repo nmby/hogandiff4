7eb27f5560e16da83adbb0975e45cb222c8c364a
nmby <nmby@hotchpotch.xyz>
2024-06-16 16:38:27 +0900

    組み合わせ編集機能の作成

--- xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/gui/dialogs/EditSheetPairingDialogPane.java
+++ xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/gui/dialogs/EditSheetPairingDialogPane.java
@@ -9,6 +9,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import xyz.hotchpotch.hogandiff.AppMain;
@@ -76,17 +81,19 @@ public class EditSheetPairingDialogPane extends VBox {
         for (int i = 0; i < currentPairs.size(); i++) {
             Pair<String> pair = currentPairs.get(i);
             
-            gridPane.add(pair.hasA()
-                    ? new NameLabel(i, Side.A, pair.a())
-                    : new DummyLabel(i, Side.A),
-                    0, i);
             if (pair.isPaired()) {
+                gridPane.add(new PairedNameLabel(i, Side.A, pair.a()), 0, i);
                 gridPane.add(new UnpairButton(i), 1, i);
+                gridPane.add(new PairedNameLabel(i, Side.B, pair.b()), 2, i);
+                
+            } else if (pair.hasA()) {
+                gridPane.add(new UnpairedNameLabel(i, Side.A, pair.a()), 0, i);
+                gridPane.add(new DummyLabel(i, Side.B), 2, i);
+                
+            } else {
+                gridPane.add(new DummyLabel(i, Side.A), 0, i);
+                gridPane.add(new UnpairedNameLabel(i, Side.B, pair.b()), 2, i);
             }
-            gridPane.add(pair.hasB()
-                    ? new NameLabel(i, Side.B, pair.b())
-                    : new DummyLabel(i, Side.B),
-                    2, i);
         }
     }
     
@@ -102,11 +109,36 @@ public class EditSheetPairingDialogPane extends VBox {
         drawGrid();
     }
     
+    private void makePair(int src, int dst) {
+        assert src != dst;
+        assert 0 <= src && src < currentPairs.size();
+        assert 0 <= dst && dst < currentPairs.size();
+        
+        Pair<String> srcPair = currentPairs.get(src);
+        Pair<String> dstPair = currentPairs.get(dst);
+        assert !srcPair.isPaired();
+        assert !dstPair.isPaired();
+        assert srcPair.hasA() != srcPair.hasB();
+        assert dstPair.hasA() != dstPair.hasB();
+        assert srcPair.hasA() == dstPair.hasB();
+        assert srcPair.hasB() == dstPair.hasA();
+        
+        Pair<String> paired = Pair.of(
+                srcPair.hasA() ? srcPair.a() : dstPair.a(),
+                srcPair.hasB() ? srcPair.b() : dstPair.b());
+        
+        currentPairs.remove(dst);
+        currentPairs.add(dst, paired);
+        currentPairs.remove(src);
+        
+        drawGrid();
+    }
+    
     public BookCompareInfo getResult() {
         return BookCompareInfo.of(bookInfoPair, currentPairs);
     }
     
-    private class NameLabel extends Label {
+    private class PairedNameLabel extends Label {
         
         // static members ------------------------------------------------------
         
@@ -115,10 +147,47 @@ public class EditSheetPairingDialogPane extends VBox {
         private final int idx;
         private final Side side;
         
-        private NameLabel(int idx, Side side, String name) {
+        private PairedNameLabel(int idx, Side side, String name) {
             this.idx = idx;
             this.side = side;
             setText(name);
+            getStyleClass().add("nameLabel");
+            getStyleClass().add("pairedNameLabel");
+        }
+    }
+    
+    private class UnpairedNameLabel extends Label {
+        
+        // static members ------------------------------------------------------
+        
+        // instance members ----------------------------------------------------
+        
+        private final int idx;
+        private final Side side;
+        private final String name;
+        
+        private UnpairedNameLabel(int idx, Side side, String name) {
+            this.idx = idx;
+            this.side = side;
+            this.name = name;
+            setText(name);
+            getStyleClass().add("nameLabel");
+            getStyleClass().add("unpairedNameLabel");
+            setOnDragDetected(this::onDragDetected);
+        }
+        
+        private void onDragDetected(MouseEvent event) {
+            try {
+                event.consume();
+                Dragboard board = startDragAndDrop(TransferMode.MOVE);
+                ClipboardContent content = new ClipboardContent();
+                content.putString("%s%d".formatted(side, idx));
+                board.setContent(content);
+                
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+                // nop
+            }
         }
     }
     
@@ -134,6 +203,73 @@ public class EditSheetPairingDialogPane extends VBox {
         private DummyLabel(int idx, Side side) {
             this.idx = idx;
             this.side = side;
+            setText("(dummy)");
+            getStyleClass().add("dummyLabel");
+            setOnDragOver(this::onDragOver);
+            setOnDragDropped(this::onDragDropped);
+        }
+        
+        private Integer getIdx(String id) {
+            if (id == null || id.length() < 2) {
+                return null;
+            }
+            try {
+                Side side = Side.valueOf(id.substring(0, 1));
+                int idx = Integer.parseInt(id.substring(1));
+                if (side != this.side || idx == this.idx) {
+                    return null;
+                }
+                return idx;
+                
+            } catch (RuntimeException e) {
+                return null;
+            }
+        }
+        
+        private void onDragOver(DragEvent event) {
+            try {
+                event.consume();
+                
+                if (!event.getDragboard().hasString()) {
+                    return;
+                }
+                String id = event.getDragboard().getString();
+                Integer idx = getIdx(id);
+                if (idx == null) {
+                    return;
+                }
+                event.acceptTransferModes(TransferMode.MOVE);
+                
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+                // nop
+            }
+        }
+        
+        private void onDragDropped(DragEvent event) {
+            try {
+                // TODO: event.consume()の位置がおかしい気がするので見直す
+                event.consume();
+                
+                if (!event.getDragboard().hasString()) {
+                    event.setDropCompleted(false);
+                    return;
+                }
+                String id = event.getDragboard().getString();
+                Integer idx = getIdx(id);
+                if (idx == null) {
+                    event.setDropCompleted(false);
+                    return;
+                }
+                
+                makePair(idx, this.idx);
+                event.setDropCompleted(true);
+                
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+                event.setDropCompleted(false);
+                // nop
+            }
         }
     }
     
@@ -147,6 +283,7 @@ public class EditSheetPairingDialogPane extends VBox {
         
         private UnpairButton(int idx) {
             this.idx = idx;
+            getStyleClass().add("unpairButton");
             
             setOnAction(event -> unpair(idx));
         }
