a2fed83ad9a1e895c052ad38c84661e8e53bca10
nmby <namba.yasuhiro.1@gmail.com>
2025-09-08 09:13:55 +0900

    詳細設定ダイアログの導入

--- xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/gui/components/SettingsPane2.java
+++ xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/gui/components/SettingsPane2.java
@@ -5,35 +5,28 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Locale;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.stream.Stream;
 
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Dialog;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
-import javafx.util.Callback;
 import xyz.hotchpotch.hogandiff.AppMain;
 import xyz.hotchpotch.hogandiff.AppResource;
 import xyz.hotchpotch.hogandiff.SettingKeys;
 import xyz.hotchpotch.hogandiff.gui.ChildController;
 import xyz.hotchpotch.hogandiff.gui.MainController;
+import xyz.hotchpotch.hogandiff.gui.dialogs.SettingDetailsDialogPane;
 import xyz.hotchpotch.hogandiff.util.function.UnsafeConsumer;
 
 /**
@@ -45,41 +38,6 @@ public class SettingsPane2 extends VBox implements ChildController {
     
     // [static members] ********************************************************
     
-    private static enum LocaleItem {
-        
-        // [static members] ----------------------------------------------------
-        
-        /** 日本語 */
-        JA("日本語", Locale.JAPANESE, "jp.png"),
-        
-        /** 英語 */
-        EN("English", Locale.ENGLISH, "us.png"),
-        
-        /** 中国語（簡体字） */
-        ZH("簡体中文", Locale.SIMPLIFIED_CHINESE, "cn.png");
-        
-        public static LocaleItem of(Locale locale) {
-            Objects.requireNonNull(locale);
-            
-            return Stream.of(values())
-                    .filter(item -> item.locale == locale)
-                    .findFirst()
-                    .orElseThrow();
-        }
-        
-        // [instance members] --------------------------------------------------
-        
-        private final String text;
-        private final Locale locale;
-        private final Image image;
-        
-        LocaleItem(String text, Locale locale, String imageSrc) {
-            this.text = text;
-            this.locale = locale;
-            this.image = new Image(imageSrc);
-        }
-    }
-    
     // [instance members] ******************************************************
     
     private final AppResource ar = AppMain.appResource;
@@ -89,9 +47,6 @@ public class SettingsPane2 extends VBox implements ChildController {
     private GooglePane googlePane;
     
     @FXML
-    private ComboBox<LocaleItem> localeComboBox;
-    
-    @FXML
     private Button openWorkDirButton;
     
     @FXML
@@ -99,6 +54,9 @@ public class SettingsPane2 extends VBox implements ChildController {
     
     @FXML
     private Button deleteWorkDirButton;
+    
+    @FXML
+    private Button detailsButton;
     
     /**
      * コンストラクタ<br>
@@ -121,30 +79,29 @@ public class SettingsPane2 extends VBox implements ChildController {
         
         // 2.項目ごとの各種設定
         googlePane.init(parent);
-        localeComboBox.setItems(FXCollections.observableArrayList(LocaleItem.values()));
-        localeComboBox.setButtonCell(cellFactory(false).call(null));
-        localeComboBox.setCellFactory(cellFactory(true));
         
         openWorkDirButton.setOnAction(openDir);
         changeWorkDirButton.setOnAction(changeDir);
         deleteWorkDirButton.setOnAction(deleteDir);
         
-        localeComboBox.setOnAction(event -> {
-            if (ar.changeSetting(SettingKeys.APP_LOCALE, localeComboBox.getValue().locale)) {
-                new Alert(
-                        AlertType.INFORMATION,
-                        "%s%n%n%s%n%n%s".formatted(
-                                rb.getString("gui.component.SettingsPane2.051"),
-                                rb.getString("gui.component.SettingsPane2.052"),
-                                rb.getString("gui.component.SettingsPane2.053")),
-                        ButtonType.OK)
-                                .showAndWait();
+        detailsButton.setOnAction(event -> {
+            try {
+                SettingDetailsDialogPane detailsContent = new SettingDetailsDialogPane();
+                detailsContent.init();
+                Dialog<Void> detailsDialog = new Dialog<>();
+                detailsDialog.setTitle("★方眼Diff  -  詳細設定");
+                detailsDialog.getDialogPane().setContent(detailsContent);
+                detailsDialog.getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
+                detailsDialog.showAndWait();
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+                // nop
             }
         });
         
         // 3.初期値の設定
-        Locale locale = ar.settings().get(SettingKeys.APP_LOCALE);
-        localeComboBox.setValue(LocaleItem.of(locale));
+        // nop
         
         // 4.値変更時のイベントハンドラの設定
         // nop
@@ -248,29 +205,4 @@ public class SettingsPane2 extends VBox implements ChildController {
             });
         }
     };
-    
-    private Callback<ListView<LocaleItem>, ListCell<LocaleItem>> cellFactory(boolean showText) {
-        return listView -> new ListCell<>() {
-            @Override
-            public void updateItem(LocaleItem item, boolean empty) {
-                super.updateItem(item, empty);
-                
-                if (empty || item == null) {
-                    setText(null);
-                    setGraphic(null);
-                } else {
-                    ImageView iv = new ImageView(item.image);
-                    iv.setFitHeight(17);
-                    iv.setPreserveRatio(true);
-                    setGraphic(iv);
-                    
-                    if (showText) {
-                        setText(item.text);
-                    } else {
-                        this.setAlignment(Pos.CENTER);
-                    }
-                }
-            }
-        };
-    }
 }
---
+++ xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/gui/dialogs/SettingDetailsDialogPane.java
@@ -0,0 +1,140 @@
+package xyz.hotchpotch.hogandiff.gui.dialogs;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.stream.Stream;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.util.Callback;
+import xyz.hotchpotch.hogandiff.AppMain;
+import xyz.hotchpotch.hogandiff.AppResource;
+import xyz.hotchpotch.hogandiff.SettingKeys;
+
+public class SettingDetailsDialogPane extends VBox {
+    
+    // [static members] ********************************************************
+    
+    private static final AppResource ar = AppMain.appResource;
+    private static final ResourceBundle rb = ar.get();
+    
+    private static enum LocaleItem {
+        
+        // [static members] ----------------------------------------------------
+        
+        /** 日本語 */
+        JA("日本語", Locale.JAPANESE, "jp.png"),
+        
+        /** 英語 */
+        EN("English", Locale.ENGLISH, "us.png"),
+        
+        /** 中国語（簡体字） */
+        ZH("簡体中文", Locale.SIMPLIFIED_CHINESE, "cn.png");
+        
+        public static LocaleItem of(Locale locale) {
+            Objects.requireNonNull(locale);
+            
+            return Stream.of(values())
+                    .filter(item -> item.locale == locale)
+                    .findFirst()
+                    .orElseThrow();
+        }
+        
+        // [instance members] --------------------------------------------------
+        
+        private final String text;
+        private final Locale locale;
+        private final Image image;
+        
+        LocaleItem(String text, Locale locale, String imageSrc) {
+            this.text = text;
+            this.locale = locale;
+            this.image = new Image(imageSrc);
+        }
+    }
+    
+    // [instance members] ******************************************************
+    
+    @FXML
+    private ComboBox<LocaleItem> localeComboBox;
+    
+    /**
+     * コンストラクタ<br>
+     * 
+     * @throws IOException FXMLファイルの読み込みに失敗した場合
+     */
+    public SettingDetailsDialogPane() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("SettingDetailsDialogPane.fxml"), rb);
+        loader.setRoot(this);
+        loader.setController(this);
+        loader.load();
+    }
+    
+    public void init() {
+        // 1.disableプロパティのバインディング
+        // nop
+        
+        // 2.項目ごとの各種設定
+        localeComboBox.setItems(FXCollections.observableArrayList(LocaleItem.values()));
+        localeComboBox.setButtonCell(cellFactory(false).call(null));
+        localeComboBox.setCellFactory(cellFactory(true));
+        
+        localeComboBox.setOnAction(event -> {
+            if (ar.changeSetting(SettingKeys.APP_LOCALE, localeComboBox.getValue().locale)) {
+                new Alert(
+                        AlertType.INFORMATION,
+                        "%s%n%n%s%n%n%s".formatted(
+                                rb.getString("gui.component.SettingsPane2.051"),
+                                rb.getString("gui.component.SettingsPane2.052"),
+                                rb.getString("gui.component.SettingsPane2.053")),
+                        ButtonType.OK)
+                                .showAndWait();
+            }
+        });
+        
+        // 3.初期値の設定
+        Locale locale = ar.settings().get(SettingKeys.APP_LOCALE);
+        localeComboBox.setValue(LocaleItem.of(locale));
+        
+        // 4.値変更時のイベントハンドラの設定
+        // nop
+    }
+    
+    private Callback<ListView<LocaleItem>, ListCell<LocaleItem>> cellFactory(boolean showText) {
+        return listView -> new ListCell<>() {
+            @Override
+            public void updateItem(LocaleItem item, boolean empty) {
+                super.updateItem(item, empty);
+                
+                if (empty || item == null) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    ImageView iv = new ImageView(item.image);
+                    iv.setFitHeight(17);
+                    iv.setPreserveRatio(true);
+                    setGraphic(iv);
+                    
+                    if (showText) {
+                        setText(item.text);
+                    } else {
+                        this.setAlignment(Pos.CENTER);
+                    }
+                }
+            }
+        };
+    }
+}
