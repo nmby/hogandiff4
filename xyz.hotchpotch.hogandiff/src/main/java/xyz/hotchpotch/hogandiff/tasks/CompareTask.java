3eab6672383dd4c523ab7a41f469b2b87dc7d98a
nmby <namba.yasuhiro.1@gmail.com>
2025-10-19 11:31:44 +0900

    パスワード不明ペアはスキップすることにより不要な例外スローを回避

--- xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/tasks/CompareTask.java
+++ xyz.hotchpotch.hogandiff/src/main/java/xyz/hotchpotch/hogandiff/tasks/CompareTask.java
@@ -625,7 +625,7 @@ import xyz.hotchpotch.hogandiff.util.Settings;
                 if (bookInfoPair.a().status() == Status.NEEDS_PASSWORD
                         || bookInfoPair.b().status() == Status.NEEDS_PASSWORD) {
                     
-                    str.append(" - ★★★パスワード不明のためスキップ★★★\n");
+                    str.append(" - ").append(Msg.APP_1300).append(BR);
                     updateMessage(str.toString());
                     
                     bookResults.put(bookInfoPair, Optional.empty());
